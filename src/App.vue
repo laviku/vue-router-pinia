6ed7564b847d8229b74a8bd9fc4be743f3ad57af
lavinia-ns <laviniam@nicasource.com>
2024-09-01 18:19:17 -0600

    setup

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,9 @@ import { RouterView } from 'vue-router'
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  @apply bg-neutral-900 text-gray-200;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,27 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    children: [
+      {
+        path: ':id(\\d+)',
+        component: () => import('../views/MessagesView.vue')
+      },
+      {
+        path: ':(.*)',
+        component: () => import('../views/VoidMessagesView.vue')
+      }
+    ]
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
